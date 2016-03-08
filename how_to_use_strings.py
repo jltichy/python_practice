a70fdda3bf71b8d34e3a4ac23854d13000d227cf
jltichy <jltichy@gmail.com>
2016-03-08 22:39:49 +0000

    Added a file

--- how_to_use_strings.py
+++ how_to_use_strings.py
@@ -11,7 +11,7 @@ my_hair = 'Brown'
 print "Let's talk about %s." % my_name
 print "She's %d inches tall." % my_height
 print "She's %d pounds heavy." % my_weight
-print "Actually, that's not that heavy."
+print "Actually, that's not too heavy."
 print "She has %s eyes and %s hair." % (my_eyes, my_hair)
 print "She has %s teeth, except if she drinks too much coffee." % my_teeth
 print "If I add %d, %d, and %d, I get %d." % (my_age, my_height, my_weight, my_age + my_height + my_weight)
