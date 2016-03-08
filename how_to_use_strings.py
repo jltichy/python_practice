ebcd8c017a20096a02ef6475c12805c3db3ed571
jltichy <jltichy@gmail.com>
2016-03-08 14:35:59 -0700

    Create how_to_use_strings.py

---
+++ how_to_use_strings.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python 
+
+my_name = 'Jennifer Tichy'
+my_age = 33 #only for about a month; then it's my birthday!
+my_height = 12*5+7 #I was too lazy to calculate this with a calculator, but since Python can calculate things, here we go.
+my_weight = 135 #relatively consistent
+my_eyes = 'Blue'
+my_teeth = 'White'
+my_hair = 'Brown'
+
+print "Let's talk about %s." % my_name
+print "She's %d inches tall." % my_height
+print "She's %d pounds heavy." % my_weight
+print "Actually, that's not that heavy."
+print "She has %s eyes and %s hair." % (my_eyes, my_hair)
+print "She has %s teeth, except if she drinks too much coffee." % my_teeth
+print "If I add %d, %d, and %d, I get %d." % (my_age, my_height, my_weight, my_age + my_height + my_weight)
