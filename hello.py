3d3001dae042e922609e7aa56de6b34dcfecfcd5
jltichy <jltichy@gmail.com>
2016-03-09 13:15:31 -0700

    Writes to a file, but writes incorrectly.

--- hello.py
+++ hello.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python
 
+import sys
+
 print "Enter the word that you want displayed."
 word = raw_input()
 number = input('Enter the number of times that you want the word displayed.')
@@ -8,3 +10,10 @@ count = 0
 while (count < number):
    print "%r" % (word)
    count = count + 1
+
+print "This will also be displayed in the test.txt file."
+
+f = open("test.txt", "w");
+print f
+f.write("%r" % (word))
+
