f3d66dd320bec413f677c27afefa26678dbabcc4
jltichy <jltichy@gmail.com>
2016-03-10 14:19:31 -0700

    Update hello.py

--- hello.py
+++ hello.py
@@ -1,3 +1,5 @@
+# Using a for loop, create a document that has a user-inputed word printed a certain number of times (user-inputed)
+
 #!/usr/bin/env python
 
 import sys
@@ -6,26 +8,11 @@ print "Enter the word that you want displayed."
 word = raw_input()
 number = input('Enter the number of times that you want the word displayed.')
 
-count = 0
-while (count < number):
-   print "%r" % (word)
-   count = count + 1
-
-print "This will also be displayed in the test.txt file."
+print "This information will be displayed in the test.txt file."
 
 f = open("test.txt", "w");
-print f
-f.write("%r" % (word))
-#f.write(for (count < number): print "%r" % (word) count = count + 1)
 
-
-# Here is Adam's code:
-f = open('blah','w')
-
-f.write('Hello kitty\n')
-
-f.close()
-
-
-print 'Bam bam' + str(5) + ' bam bam' + str(4.5) + '\n'
-
+for x in range(0, number):
+    f.write("word" + '\n')
+    
+    f.close()
