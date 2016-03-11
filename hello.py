b65a09bf18aa54f3434ff76f1ef4447331f97471
jltichy <jltichy@gmail.com>
2016-03-11 09:09:03 -0700

    Update hello.py
    Comments were added.

--- hello.py
+++ hello.py
@@ -6,23 +6,23 @@
 # import sys
 # This doesn't need to be included in this code.
 
-print "Enter the word that you want displayed."
-word = raw_input()
+print "Enter the word that you want displayed." # This will be printed on the screen.
+word = raw_input() # This sets the variable "word" to be whatever the user inputs.
 
-while True:
+while True: # This statement deals with user-error.
    try:
       number = input('Enter the number of times that you want the word displayed.') # The user must enter the number, not the number written as a word (e.g. five won't work)
       break
    except NameError:
-      print "Oops!  That was not a valid number.  Try again..."
+      print "Oops!  That was not a valid number.  Try again..." # This statement tells the user that s/he did something wrong.
 
 print "This information will be displayed in the test.txt file."
 
-f = open("test.txt", "w");
+f = open("test.txt", "w"); # This opens a file called "test.txt" and will write to it (w).
 
-for x in range(0, number):
-    f.write(word + '\n')
-f.close()
+for x in range(0, number): # This tells the for loop how many times to run (up to the user-entered number)
+    f.write(word + '\n') # The \n at the end starts a new line every time the word is printed in the document.
+f.close() # This closes the test.txt file.  Note that it is not indented, because you don't want to close the file every time a word is printed in the document.
 
 """Okay,
 
