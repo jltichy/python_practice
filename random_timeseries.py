870a114a47580c9b1c857fac4ae25fbfd42bade2
jltichy <jltichy@gmail.com>
2016-03-15 09:04:18 -0600

    Update random_timeseries.py

--- random_timeseries.py
+++ random_timeseries.py
@@ -2,3 +2,14 @@
 
 # Instructions - Create an array of 1000 randomly distributed numbers.  Plot these as a timeseries from 1 to 1000.  Use numpy and matlibplot.
 
+# This is how to install Matlibplot - sudo apt-get install python-matplotlib
+
+import numpy as np # np is the name of the matrix to be created
+import random
+matrix = np.zeros((1000, 1)) # this creates a matrix of zeros with 1 column and 1000 rows
+print matrix # allows us to see what's in the matrix (all zeros at this point)
+matrix = np.random.randint(0, 10, (1000, 1)) # fills the matrix of zeros with random integers from 0 to 10.  The size of the matrix doesn't change.
+print matrix # allows us to see what's in the matrix of random values
+
+time = np.arange(1,1001) # creates an array of values from 1 to 1000.  This will be the timeseries (x-axis).
+print time
