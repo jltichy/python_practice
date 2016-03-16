d3bc12350d7c64aea3a6e630b517d4578da8a0ba
jltichy <jltichy@gmail.com>
2016-03-16 11:34:18 -0600

    Update random_timeseries.py
    This is almost complete.  I need to add axis labels and a comment/description so the plot stands alone.  Also, since I am saving the plot in my directory on Cloud9, I need to figure out how to get this back over here.

--- random_timeseries.py
+++ random_timeseries.py
@@ -6,18 +6,20 @@
 
 import numpy as np # np is the name of the matrix to be created
 import random
-
+import pprint
 import matplotlib
 matplotlib.use('Agg')
 import matplotlib.pyplot as plt # Do not do this prior to calling use()
 
-matrix = np.zeros((1000, 1)) # this creates a matrix of zeros with 1 column and 1000 rows
-#print matrix # allows us to see what's in the matrix (all zeros at this point)
-matrix = np.random.randint(0, 10, (1000, 1)) # fills the matrix of zeros with random integers from 0 to 10.  The size of the matrix doesn't change.
-#print matrix # allows us to see what's in the matrix of random values
+matrix = np.zeros((1, 1000)) # this creates a matrix of zeros with 1 column and 1000 rows
+print matrix # allows us to see what's in the matrix (all zeros at this point)
+matrix = np.random.randint(0, 100, (1, 1000)) # fills the matrix of zeros with random integers from 0 to 10.  The size of the matrix doesn't change.
+pprint.pprint(matrix)
+print matrix # allows us to see what's in the matrix of random values
 
 time = np.arange(1,1001)
-#print time
+pprint.pprint(time)
+print time
 
 # x = time
 # y = matrix
