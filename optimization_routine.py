72dae8bb073f2a333510c55decdc77f4777c9952
jltichy <jltichy@gmail.com>
2016-03-18 15:48:36 +0000

    comments have been added for the first half of the exercise

--- optimization_routine.py
+++ optimization_routine.py
@@ -1,16 +1,27 @@
 #!/usr/bin/env python
 
 # Optimization routine
-from scipy.optimize import fmin
+from scipy.optimize import fmin 
+#scipy is a package for high level scientific computing, such as interpolation, integration, optimization, image processing, and statistics
+#Optimization is the problem of finding a numerical solution to a minimization or equality.
+#fmin is the minimum of a function
+
 
 # A function 
 def func(x):
-    return abs(x**2 -1.)
+    return abs(x**2 -1.) 
+#def allows you to define a function of your own, in this case a function of x
+#abs is absolute value
+#x**2 means "x-squared"
 
 # Forward problem: given x compute func(x)
 # The value of our function at 1
 print 'Here is our function at 1: ' + str(func(1.))
+#one squared minus one should be zero
 
+# Let's try it with another value:
+# The value of our function at 5
+print 'Here is our function at 5: ' + str(func(5))
 
 # Inverse problem: given y find x such that func(x)=y
 # We want to solve for where our function = 0
