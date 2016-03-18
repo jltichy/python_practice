83eda301e6598b9c63421ecf814d274b95b1c0f7
jltichy <jltichy@gmail.com>
2016-03-18 10:17:51 -0600

    Update optimization_routine.py

--- optimization_routine.py
+++ optimization_routine.py
@@ -26,13 +26,26 @@ print 'Here is our function at 5: ' + str(func(5))
 # Inverse problem: given y find x such that func(x)=y
 # We want to solve for where our function = 0
 # -4 is the initial guess, xtol and ftol are convergence parameters
+blah = fmin(func,-4., xtol=10**-9, ftol=10**-9) #this is the original code
+print 'Here is the solution to our inverse problem: ' + str(blah)
+#Optimization terminated successfully.                                                                                                                   
+#        Current function value: 0.000000                                                                                                               
+#        Iterations: 36                                                                                                                                 
+#        Function evaluations: 73                                                                                                                       
+#Here is the solution to our inverse problem: [-1.]  
+
+# Let's try to start with a guess of 4 instead of -4
 blah = fmin(func,-4., xtol=10**-9, ftol=10**-9)
 print 'Here is the solution to our inverse problem: ' + str(blah)
-
+#Optimization terminated successfully.                                                                                                                   
+#        Current function value: 0.000000                                                                                                               
+#        Iterations: 36                                                                                                                                 
+#        Function evaluations: 73                                                                                                                       
+#Here is the solution to our inverse problem: [-1.] 
 
 # Tasks
-# 1: Read about fmin and def and functions
-# 2: What happens when you start with a guess of 4 and not -4?
+# 1: Read about fmin and def and functions - done. see comments above
+# 2: What happens when you start with a guess of 4 and not -4? done. see results above.  You get the same answer because of the absolute value command.
 # 3: Write a function that takes a list and computes the sum of the squares 
 # 	e.g. x(1)**2 + x(2)**2 + ... + x(n)**2
 # 4: Use a list of two elements and find the minimum of the function in 3
