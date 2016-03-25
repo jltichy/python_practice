c25b7de5601d480c6d4bf8f1b1f788ec630e25e7
jltichy <jltichy@gmail.com>
2016-03-25 15:48:19 +0000

    update exercises

--- classes_instances.py
+++ classes_instances.py
@@ -9,7 +9,8 @@ var = 'Hello World!' #var is a string, so it is a string object
 print type(var)
 print var.upper() #prints the variable in all upper case letters
 
-class Car(object):
+# Here is some example code, but it doesn't work right now.  It needs to be fixed.
+'''class Car(object):
 redcar = Car('red') #Car is the class, and here we are passing a color value
 bluecar = Car('blue') 
 
@@ -19,4 +20,22 @@ redcar.start()
 
 bluecar.start()
 
-redcar.stop()
+redcar.stop()'''
+
+class Chat:
+    name = 'Jennifer'
+    def insert(self):
+        print 'We inserted something.'
+    def delete(self):
+        print 'We deleted something.'
+        
+c = Chat()
+print c # <__main__.Chat instance at 0x7fcc0976ea70> # Notice this value compared to the one below for d -- even though Chat() is the same thing, it is used for a different instance between c and d.
+d = Chat()
+print d # <__main__.Chat instance at 0x7fcc0976eb00>
+
+c.insert() # prints 'We inserted something.' # Use the dot notation to go into objects.  Here the c is the instance and the insert is the method.
+c.delete() # prints 'We deleted something.'
+c.name # should print 'Jennifer' but for some reason this isn't working.  I'm not sure what I'm doing wrong here.
+
+
