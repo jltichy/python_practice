8b18474b3b6e30ff4e711af5e1ed32b47239c9b5
jltichy <jltichy@gmail.com>
2016-03-25 16:11:42 +0000

    complete tutorial examples

--- classes_instances.py
+++ classes_instances.py
@@ -36,6 +36,31 @@ print d # <__main__.Chat instance at 0x7fcc0976eb00>
 
 c.insert() # prints 'We inserted something.' # Use the dot notation to go into objects.  Here the c is the instance and the insert is the method.
 c.delete() # prints 'We deleted something.'
-c.name # should print 'Jennifer' but for some reason this isn't working.  I'm not sure what I'm doing wrong here.
+print c.name # if only put c.name, it doesn't print 'Jennifer' to the screen, but if put print c.name, it does
+
+c.name = 'Adam' 
+print c.name # prints Adam; note that we were able to overwrite using this object oriented programming.
+
+
+
+# Let's create another variable within our class.
+
+class Chat2:
+    name2 = 'Jennifer2'
+    def insert2(self2):
+        print 'We inserted something.'
+    def delete2(self2):
+        print 'We deleted something.'
+    def change2(self2,value2):
+        self2.name2 = value2
+
+c2 = Chat2()
+print c2 # <__main__.Chat2 instance at 0x7f77b696edd0>
+print c2.name2 # prints 'Jennifer2'
+print c2.change2('Adam2') # prints 'None', but then:
+print c2.name2 # prints 'Adam2'
+
+# This allows us to manipulate values!  Cool!
+
 
 
