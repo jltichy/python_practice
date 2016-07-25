b64f1ef25d4d64cf79959bff77a0b3e34927f09f
jltichy <jltichy@gmail.com>
2016-07-25 15:22:49 -0600

    Create class_practice.py

---
+++ class_practice.py
@@ -0,0 +1,8 @@
+class Rocket():
+    # Rocket simulates a rocket ship for a game,
+    #  or a physics simulation.
+    
+    def __init__(self):
+        # Each rocket has an (x,y) position.
+        self.x = 0
+        self.y = 0
