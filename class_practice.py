5b9ce3c08b059b39877bd958392354e81a11fdbf
jltichy <jltichy@gmail.com>
2016-07-25 21:57:14 +0000

    update practice file

--- class_practice.py
+++ class_practice.py
@@ -8,3 +8,17 @@ class Rocket():
         # Each rocket has an (x,y) position.
         self.x = 0
         self.y = 0
+        
+    def move_up(self):
+        # Increment the y-position of the rocket.
+        self.y += 1
+        
+# Create a fleet of 5 rockets, and store them in a list.
+my_rockets = [Rocket() for x in range(0,5)]
+
+# Move the first rocket up.
+my_rockets[0].move_up()
+
+# Show that only the first rocket has moved.
+for rocket in my_rockets:
+    print("Rocket altitude:", rocket.y)
