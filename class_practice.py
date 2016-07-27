43d46652f2b7d44c8ac3d0aea326167fb0ee9bcc
jltichy <jltichy@gmail.com>
2016-07-27 22:41:40 +0000

    update file and fix adam's additions

--- class_practice.py
+++ class_practice.py
@@ -17,7 +17,8 @@ class Rocket(object):
         
 # Create a fleet of 5 rockets, and store them in a list.
 my_rockets = [Rocket() for x in range(0,5)]
-print my_rockets
+for rocket in my_rockets:
+    print(rocket)
 
 # Move the first rocket up.
 my_rockets[0].move_up()
@@ -34,38 +35,61 @@ print("Rocket 2 Coordinates:", my_rockets[1].x,my_rockets[1].y)
 
 # Exercise: Store an x and y value for each rocket in a set of 5 rockets. Store these 5 rockets in a list.
 from random import randint
-my_rockets[0].x=randint(0,100)
-my_rockets[0].y=randint(0,100)
-print("Rocket 1 Coordinates:", my_rockets[0].x,my_rockets[0].y)
-my_rockets[1].x=randint(0,100)
-my_rockets[1].y=randint(0,100)
-print("Rocket 2 Coordinates:", my_rockets[1].x,my_rockets[1].y)
-my_rockets[2].x=randint(0,100)
-my_rockets[2].y=randint(0,100)
-print("Rocket 3 Coordinates:", my_rockets[2].x,my_rockets[2].y)
-my_rockets[3].x=randint(0,100)
-my_rockets[3].y=randint(0,100)
-print("Rocket 4 Coordinates:", my_rockets[3].x,my_rockets[3].y)
-my_rockets[4].x=randint(0,100)
-my_rockets[4].y=randint(0,100)
-print("Rocket 5 Coordinates:", my_rockets[4].x,my_rockets[4].y)
 
-# List of all coordinates:
-#all_coordinates = [Rocket() for x in range(0,5)]
-#for i in all_coordinates:
-#   print "Coordinates %str" % i
+for rocket in my_rockets:
+    rocket.x=randint(0,100)
+    rocket.y=randint(0,100)
+# make two empy lists
+xcoord =[]
+ycoord =[]
+for idx, rocket in enumerate(my_rockets):
+    print('Here is the location')
+    print("Rocket ", idx+1 , "Coordinates:", rocket.x,rocket.y)
+    xcoord.append(rocket.x)
+    ycoord.append(rocket.y)
+    
+    
+print(xcoord)
+print(ycoord)
 
-# Figure out how to do the above lines of code as a loop.  This is messy as is.
-# Plot of the coordinates:
-import matplotlib
-matplotlib.use('Agg') #This is required to run matplotlib on Google Chrome.
-import matplotlib.pyplot as plt
-x = [my_rockets[0].x, my_rockets[1].x, my_rockets[2].x, my_rockets[3].x, my_rockets[4].x]
-y = [my_rockets[0].y, my_rockets[1].y, my_rockets[2].y, my_rockets[3].y, my_rockets[4].y]
-print x
-print y
-plt.scatter(x,y)
-plt.suptitle('Rocket Coordinate Locations')
-plt.xlabel('x Coordinate')
-plt.ylabel('y Coordinate')
-plt.savefig('Rocket_Coordinates')
+for coords in zip(xcoord,ycoord):
+    print(coords)
+# my_rockets[1].x=randint(0,100)
+# my_rockets[1].y=randint(0,100)
+# print("Rocket 2 Coordinates:", my_rockets[1].x,my_rockets[1].y)
+# my_rockets[2].x=randint(0,100)
+# my_rockets[2].y=randint(0,100)
+# print("Rocket 3 Coordinates:", my_rockets[2].x,my_rockets[2].y)
+# my_rockets[3].x=randint(0,100)
+# my_rockets[3].y=randint(0,100)
+# print("Rocket 4 Coordinates:", my_rockets[3].x,my_rockets[3].y)
+# my_rockets[4].x=randint(0,100)
+# my_rockets[4].y=randint(0,100)
+# print("Rocket 5 Coordinates:", my_rockets[4].x,my_rockets[4].y)
+
+# # List of all coordinates:
+# #all_coordinates = [Rocket() for x in range(0,5)]
+# #for i in all_coordinates:
+# #   print "Coordinates %str" % i
+
+# # Figure out how to do the above lines of code as a loop.  This is messy as is.
+# # Plot of the coordinates:
+# import matplotlib
+# matplotlib.use('Agg') #This is required to run matplotlib on Google Chrome.
+# import matplotlib.pyplot as plt
+
+# x = []
+# for i  in range(0,5):
+#   x.append(my_rockets())
+# print x
+
+# x = [my_rockets[0].x, my_rockets[1].x, my_rockets[2].x, my_rockets[3].x, my_rockets[4].x]
+# y = [my_rockets[0].y, my_rockets[1].y, my_rockets[2].y, my_rockets[3].y, my_rockets[4].y]
+# print x
+# print y
+# plt.scatter(x,y)
+# plt.suptitle('Rocket Coordinate Locations')
+# plt.xlabel('x Coordinate')
+# plt.ylabel('y Coordinate')
+# plt.show()
+# plt.savefig('Rocket_Coordinates.jpg', format=jpeg)
