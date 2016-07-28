189800c27aab224d4851279430c1e9dad736f651
jltichy <jltichy@gmail.com>
2016-07-28 15:54:35 +0000

    file is complete

--- class_practice.py
+++ class_practice.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python
+
 # use this site: http://introtopython.org/classes.html#Exercises-oop
 
 class Rocket(object):
-    # Rocket simulates a rocket ship for a game,
-    #  or a physics simulation.
+    # Rocket simulates a rocket ship for a game or a physics simulation.
     # Note that object is in parentheses because this is version 2.7 of Python.
     
     def __init__(self):
@@ -12,8 +12,12 @@ class Rocket(object):
         self.y = 0
         
     def move_up(self):
-        # Increment the y-position of the rocket.
+        # Increment the y-position of the rocket, upwards.
         self.y += 1
+        
+    def move_down(self):
+        # Increment the y-position of the rocket, downwards.
+        self.y -=1
         
 # Create a fleet of 5 rockets, and store them in a list.
 my_rockets = [Rocket() for x in range(0,5)]
@@ -23,7 +27,10 @@ for rocket in my_rockets:
 # Move the first rocket up.
 my_rockets[0].move_up()
 
-# Show that only the first rocket has moved.
+# Move the second rocket down.
+my_rockets[1].move_down()
+
+# Show that only the first and second rockets have moved.
 for rocket in my_rockets:
     print("Rocket altitude:", rocket.y)
     
@@ -35,25 +42,31 @@ print("Rocket 2 Coordinates:", my_rockets[1].x,my_rockets[1].y)
 
 # Exercise: Store an x and y value for each rocket in a set of 5 rockets. Store these 5 rockets in a list.
 from random import randint
-
+# Note that Adam helped me with this loop:
 for rocket in my_rockets:
     rocket.x=randint(0,100)
     rocket.y=randint(0,100)
-# make two empy lists
+    
+# Make two empy lists
 xcoord =[]
 ycoord =[]
+
+# Note that Adam helped me with this loop as well:
+# idx is the index and it starts at 0, by default, so +1 was included for this code.
 for idx, rocket in enumerate(my_rockets):
     print('Here is the location')
     print("Rocket ", idx+1 , "Coordinates:", rocket.x,rocket.y)
     xcoord.append(rocket.x)
     ycoord.append(rocket.y)
     
-    
 print(xcoord)
 print(ycoord)
 
+# This loop zips together the x and y coordinates, to show them both as a list.
 for coords in zip(xcoord,ycoord):
     print(coords)
+    
+# Here is my original work where I did each rocket separately.
 # my_rockets[1].x=randint(0,100)
 # my_rockets[1].y=randint(0,100)
 # print("Rocket 2 Coordinates:", my_rockets[1].x,my_rockets[1].y)
@@ -67,29 +80,20 @@ for coords in zip(xcoord,ycoord):
 # my_rockets[4].y=randint(0,100)
 # print("Rocket 5 Coordinates:", my_rockets[4].x,my_rockets[4].y)
 
-# # List of all coordinates:
-# #all_coordinates = [Rocket() for x in range(0,5)]
-# #for i in all_coordinates:
-# #   print "Coordinates %str" % i
 
-# # Figure out how to do the above lines of code as a loop.  This is messy as is.
-# # Plot of the coordinates:
-# import matplotlib
-# matplotlib.use('Agg') #This is required to run matplotlib on Google Chrome.
-# import matplotlib.pyplot as plt
+# Plot of the coordinates:
+import matplotlib
+matplotlib.use('Agg') #This is required to run matplotlib on Google Chrome.
+import matplotlib.pyplot as plt
 
-# x = []
-# for i  in range(0,5):
-#   x.append(my_rockets())
-# print x
+#x = [my_rockets[0].x, my_rockets[1].x, my_rockets[2].x, my_rockets[3].x, my_rockets[4].x]
+#y = [my_rockets[0].y, my_rockets[1].y, my_rockets[2].y, my_rockets[3].y, my_rockets[4].y]
+#print x
+#print y
 
-# x = [my_rockets[0].x, my_rockets[1].x, my_rockets[2].x, my_rockets[3].x, my_rockets[4].x]
-# y = [my_rockets[0].y, my_rockets[1].y, my_rockets[2].y, my_rockets[3].y, my_rockets[4].y]
-# print x
-# print y
-# plt.scatter(x,y)
-# plt.suptitle('Rocket Coordinate Locations')
-# plt.xlabel('x Coordinate')
-# plt.ylabel('y Coordinate')
-# plt.show()
-# plt.savefig('Rocket_Coordinates.jpg', format=jpeg)
+plt.scatter(xcoord,ycoord)
+plt.suptitle('Rocket Coordinate Locations')
+plt.xlabel('x Coordinate')
+plt.ylabel('y Coordinate')
+plt.show()
+plt.savefig('Rocket_Coordinates.jpg', format='jpeg')
