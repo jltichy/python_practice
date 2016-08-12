c529f1022d52f8a57931ae3d5f53582f491fc4fe
jltichy <jltichy@gmail.com>
2016-08-12 13:44:10 +0000

    trying to convert

--- dads_files.py
+++ dads_files.py
@@ -1,3 +1,9 @@
 #!/usr/bin/env python
 
-read.table("plot0001.plt", header = TRUE, quote = "\"", skip = 7, sep = ",")
+import numpy as np
+import matplotlib.pyplot as plt
+
+f = open('plot0001.plt', 'r')
+data = f.read()
+
+#f.read("plot0001.plt", header = TRUE, quote = "\"", skip = 7, sep = ",")
