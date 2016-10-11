c26ab005e7ccbfd524361ee948f109c0de8ecd90
jltichy <jltichy@gmail.com>
2016-10-11 21:56:05 +0000

    reverse letters

--- reverse_string.py
+++ reverse_string.py
@@ -1,4 +1,9 @@
 #!/usr/bin/env python
 
-# Ask the user for a string.  Write a program to reverse the words in that string.
+# Ask the user for a string.  Write a program to reverse the words in that string
 
+directions = raw_input("Please enter a string, aka a sentence.")
+print directions
+
+reversed_string = ' '.join(reversed(directions))
+print reversed_string
