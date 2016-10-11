8bbd4e09271a649e9e765faf1e263dcd06d9ec01
jltichy <jltichy@gmail.com>
2016-10-11 22:15:23 +0000

    reverse letters and words in user-inputed string

--- reverse_string.py
+++ reverse_string.py
@@ -2,8 +2,21 @@
 
 # Ask the user for a string.  Write a program to reverse the words in that string
 
+# This part will reverse the letters in the string.
 directions = raw_input("Please enter a string, aka a sentence.")
 print directions
 
 reversed_string = ' '.join(reversed(directions))
 print reversed_string
+
+# This part will reverse the words in the string.
+splitted = directions.split()
+print splitted
+reversed = splitted[::-1]
+print reversed
+result = " ".join(reversed)
+print result
+# result = " ".join(str.split()[::-1])
+# print result
+
+
