faf5ca2eaa13409078bd87c09feba0952d954e20
jltichy <jltichy@gmail.com>
2016-10-14 10:47:52 -0600

    Create hangman_exercise.py

---
+++ hangman_exercise.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+
+In this exercise, the task is to write a function that picks 
+a random word from a list of words from the SOWPODS dictionary. 
+Download this file and save it in the same directory as your 
+Python code. This file is Peter Norvig’s compilation of the 
+dictionary of words used in professional Scrabble tournaments. 
+Each line in the file contains a single word.
+
+Hint: use the Python random library for picking a random word.
