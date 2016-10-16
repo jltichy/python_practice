1ad2e5a9985db977df0f04727c39c2d69cf746d0
jltichy <jltichy@gmail.com>
2016-10-16 12:46:48 +0000

    work on this exercise

--- try_except_ex.py
+++ try_except_ex.py
@@ -1,3 +1,11 @@
 #!/usr/bin/env python
 
-# Exercise - Create a matrix of random numbers.  Take the square root of every number and only display the ones that are whole numbers.
+# Exercise - Create a matrix of random numbers.  Take the square root of every 
+# number and only display the ones that are whole numbers.
+
+import numpy as np
+import random
+from random import randint
+
+matrix = np.random.randint(100000000, size=100000000)
+
