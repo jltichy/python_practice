7d4d2136c69695108c88245174ea433def7b0851
jltichy <jltichy@gmail.com>
2016-10-17 20:34:44 +0000

    trying second example.

--- try_except_ex.py
+++ try_except_ex.py
@@ -41,4 +41,14 @@ def function():
         print('not an integer')
 
 
+# Let's try another example here.
+while True:
+    try:
+        x = int(input("Please enter a number:"))
+        break
+    except ValueError:
+        print("Oops!  That is not a valid number.  Try again.")
+        
+
+
   
