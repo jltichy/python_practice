f5949cf6ff8492c0f77a008183e2e2a07d022f04
jltichy <jltichy@gmail.com>
2016-10-18 14:59:05 +0000

    module made by me

--- jennifer.py
+++ jennifer.py
@@ -1,4 +1,7 @@
 #!/usr/bin/env python
 
+# This is an exercise to create my own module.  It is named jennifer.py
+# and it will print "Hello!" to the screen when used in another .py file.
+
 def jennifersmodule():
    print "Hello!"
