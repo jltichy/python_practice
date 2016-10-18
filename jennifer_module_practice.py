0ad49f1f7952f3c2aca45aa268efe7d131606385
jltichy <jltichy@gmail.com>
2016-10-18 14:58:44 +0000

    practice module document

--- jennifer_module_practice.py
+++ jennifer_module_practice.py
@@ -1,4 +1,7 @@
 #!/usr/bin/env python
 
+# This is a practice document to use the module called jennifer.py
+# The jennifer.py module prints the word Hello! to the screen.
+
 import jennifer
- jennifer.jennifers_module()
+jennifer.jennifersmodule()
