d21727a2d74ba5b1a935cc0c4ce5ae3a03792bcc
jltichy <jltichy@gmail.com>
2016-10-20 16:53:25 +0000

    added utf and the zen of python

--- Haltermanpythonbook.py
+++ Haltermanpythonbook.py
@@ -1,6 +1,31 @@
 #!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# The above code that includes "utf" is for text formatting.  It allows you
+# to copy and paste text that may be a different font.
 
 import sys
+import this # This is the Zen of Python - it shows how to code in a Pythonic manner
+
+# Beautiful is better than ugly.
+# Explicit is better than implicit.
+# Simple is better than complex.
+# Complex is better than complicated.
+# Flat is better than nested.
+# Sparse is better than dense.
+# Readability counts.
+# Special cases aren't special enough to break the rules.
+# Although practicality beats purity.
+# Errors should never pass silently.
+# Unless explicitly silenced.
+# In the face of ambiguity, refuse the temptation to guess.
+# There should be one-- and preferably only one --obvious way to do it.
+# Although that way may not be obvious at first unless you're Dutch.
+# Now is better than never.
+# Although never is often better than *right* now.
+# If the implementation is hard to explain, it's a bad idea.
+# If the implementation is easy to explain, it may be a good idea.
+# Namespaces are one honking great idea -- let's do more of those!
 
 debug = True
 
